39b094b55afd1bfe6d4a03bbb770a9d185912e31
Ford Atwater <fordatwater@gmail.com>
2019-06-19 16:08:23 -0700

    rm jsonUrl fix

--- tapestry.js
+++ tapestry.js
@@ -40,8 +40,7 @@ var dataset, root, svg, links, nodes,               // Basics
 
 /* Import data from json file, then start D3 */
 
-$.getJSON(jsonUrl, function(result){
-// jQuery.get(apiUrl + "/tapestries/" + tapestryWpPostId, function(result){
+jQuery.get(apiUrl + "/tapestries/" + tapestryWpPostId, function(result){
     dataset = result;
 
     //---------------------------------------------------
