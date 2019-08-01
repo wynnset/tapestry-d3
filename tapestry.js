2ea038c0328603ab28a9ed30b793a101c5528622
kevinthewebguy <kevinwong_895@hotmail.com>
2019-08-01 10:45:03 -0700

    fix post request to put request. Add delete for root

--- tapestry.js
+++ tapestry.js
@@ -21,7 +21,7 @@ var // declared constants
     CSS_OPTIONAL_LINK = "stroke-dasharray: 30, 15;",
     TIME_BETWEEN_SAVE_PROGRESS = 5, // Means the number of seconds between each save progress call
     NODE_UNLOCK_TIMEFRAME = 2, // Time in seconds. User should be within 2 seconds of appearsAt time for unlocked nodes
-    API_PUT_METHOD = 'POST',
+    API_PUT_METHOD = 'PUT',
     USER_NODE_PROGRESS_URL = apiUrl + "/users/progress",
     USER_NODE_UNLOCKED_URL = apiUrl + "/users/unlocked",
     TAPESTRY_H5P_SETTINGS_URL = apiUrl + "/users/h5psettings",
@@ -546,6 +546,18 @@ function deleteNode() {
     console.log(nodeId);
     if (nodeId === dataset.rootId) {
         console.log("handle root node case");
+        $.ajax({
+            url: apiUrl + "/tapestries/" + tapestryWpPostId + "/nodes/" + nodeId,
+            method: 'DELETE',
+            success: function(e) {
+                dataset.nodes.splice(0, 1);
+                tapestryHideAddNodeModal();
+                redrawTapestryWithNewNode();
+            },
+            error: function(e) {
+                console.error("Error deleting root node", e);
+            }
+        });
     } else {
         var linkToBeDeleted = -1;
         for (var i = 0; i < dataset.links.length; i++) {
@@ -593,7 +605,7 @@ function deleteNode() {
                                     console.log("redrew");
                                 },
                                 error: function(e) {
-                                    console.error("Error removing link", e);
+                                    console.error("Error deleting node " + nodeId, e);
                                 }
                             });
 
